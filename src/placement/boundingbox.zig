177bef065cf002d2709bb90ad7eda6122b40df0a
Traxar <trxr2a@gmail.com>
2025-06-08 16:08:53 +0000

    make boundingbox inclusive on all sides

--- src/placement/boundingbox.zig
+++ src/placement/boundingbox.zig
@@ -6,18 +6,21 @@ pub fn Type(T: type) type {
         const Placement = PlacementType(T);
         const Position = Placement.Position;
         min: Position,
-        max: Position,
+        max: Position, //inclusive
 
-        const ones: Position = @splat(1);
-        pub const none = BoundingBox{ .min = @splat(0), .max = @splat(0) };
+        pub const none = BoundingBox{ .min = @splat(1), .max = @splat(0) };
+
+        fn empty(a: BoundingBox) bool {
+            return @reduce(.Or, a.min > a.max);
+        }
 
         pub fn add(a: *BoundingBox, b: Position) void {
-            if (@reduce(.Or, a.min >= b)) {
+            if (a.empty()) {
                 a.min = b;
-                a.max = b + ones;
+                a.max = b;
             } else {
                 a.min = @min(a.min, b);
-                a.max = @max(a.max, b + ones);
+                a.max = @max(a.max, b);
             }
         }
 
@@ -26,14 +29,13 @@ pub fn Type(T: type) type {
                 .min = @max(a.min, b.min),
                 .max = @min(a.max, b.max),
             };
-            if (@reduce(.Or, c.min >= c.max)) return null;
-            return c;
+            return if (c.empty()) null else c;
         }
 
         pub fn place(a: BoundingBox, b: Placement) BoundingBox {
             var c = BoundingBox.none;
             c.add(b.move(a.min).position);
-            c.add(b.move(a.max - ones).position);
+            c.add(b.move(a.max).position);
             return c;
         }
     };
